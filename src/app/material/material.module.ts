7e600c7d41c667f932d37cc9e2482ae06ac1c0c7
Caruunico <nicolascaruso19@outlook.com>
2022-01-28 01:07:15 -0300

    titulos en experiencia laboral y links ok

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,6 +4,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 
 
@@ -18,14 +21,18 @@ import { MatIconModule } from '@angular/material/icon';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ],
   exports: [
     CommonModule,
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule { }
